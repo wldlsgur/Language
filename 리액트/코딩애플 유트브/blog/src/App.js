3ae20361858e87b4319fe6cbfa2de7aedb6cbe18
wldlsgur <dhgg321@naver.com>
2022-10-18 23:19:40 +0900

    코딩애플 리액트 시작

---
+++ 리액트/코딩애플 유트브/blog/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let post = "강남 우동 맛집";
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
