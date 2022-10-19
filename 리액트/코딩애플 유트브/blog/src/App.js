213dde1e52f97feb8404cd9f6b937402a885b7b2
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2022-10-20 01:07:15 +0900

    리액트 state 다루기

--- 리액트/코딩애플 유트브/blog/src/App.js
+++ 리액트/코딩애플 유트브/blog/src/App.js
@@ -1,15 +1,34 @@
-import logo from "./logo.svg";
+/* eslint-disable */
 import "./App.css";
-
+import { useState } from "react";
 function App() {
-  let post = "강남 우동 맛집";
+  let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [logo, setLogo] = useState("ReactBlog");
+  let [like, setLike] = useState(0);
+
+  let 함수 = () => {
+    setLike(like + 1);
+  };
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>
+          {글제목[0]} <span onClick={함수}>👍</span> {like}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
