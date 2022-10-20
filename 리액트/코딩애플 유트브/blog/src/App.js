e9fab5b71b5a743764f0940e58d75e616134903c
wldlsgur <dhgg321@naver.com>
2022-10-20 22:26:20 +0900

    코딩애플 리액트 컴포넌트

--- 리액트/코딩애플 유트브/blog/src/App.js
+++ 리액트/코딩애플 유트브/blog/src/App.js
@@ -2,35 +2,69 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  let [글제목, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
   let [logo, setLogo] = useState("ReactBlog");
   let [like, setLike] = useState(0);
-
-  let 함수 = () => {
-    setLike(like + 1);
-  };
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
+      <button
+        onClick={() => {
+          let copySort = [...title].sort();
+          setTitle(copySort);
+        }}
+      >
+        정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...title];
+          copy[0] = "여자 코트 추천";
+          setTitle(copy);
+        }}
+      >
+        코트 성별 변경
+      </button>
       <div className="list">
         <h4>
-          {글제목[0]} <span onClick={함수}>👍</span> {like}
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {like}
         </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <Modal></Modal>
     </div>
   );
 }
-
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
 export default App;
